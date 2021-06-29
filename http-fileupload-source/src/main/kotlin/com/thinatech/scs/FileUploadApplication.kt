e6da9b34714d688f704b95868816b3e83fcea906
Mahieddine ICHIR <mahieddine.ichir@gmail.com>
2021-06-29 16:14:22 +0200

    Http Inbound to Kafka Outbound

--- http-fileupload-source/src/main/kotlin/com/thinatech/scs/FileUploadApplication.kt
+++ http-fileupload-source/src/main/kotlin/com/thinatech/scs/FileUploadApplication.kt
@@ -3,33 +3,27 @@ package com.thinatech.scs
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.core.ResolvableType
 import org.springframework.integration.config.EnableIntegration
 import org.springframework.integration.core.GenericSelector
 import org.springframework.integration.dsl.IntegrationFlow
 import org.springframework.integration.dsl.IntegrationFlows
-import org.springframework.integration.dsl.MessageChannels
+import org.springframework.integration.http.dsl.Http
 import org.springframework.integration.kafka.dsl.Kafka
 import org.springframework.integration.transformer.GenericTransformer
+import org.springframework.kafka.core.ConsumerFactory
 import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.messaging.MessageChannel
-import org.springframework.messaging.support.MessageBuilder
-import java.util.function.Function
 
 @SpringBootApplication
 @EnableIntegration
 class FileUploadApplication {
 
     @Bean
-    fun numChannel(): MessageChannel {
-        return MessageChannels.direct()
-                .datatype(String::class.java)
-                .interceptor(MyChannelInterceptor())
-                .get()
-    }
+    fun flow1(kafkaTemplate: KafkaTemplate<String, Int>,
+              consumerFactory: ConsumerFactory<String, String>): IntegrationFlow {
 
-    @Bean
-    fun flow1(kafkaTemplate: KafkaTemplate<String, Int>): IntegrationFlow {
-        return IntegrationFlows.from("numChannel")
+        return IntegrationFlows
+                .from(Http.inboundChannelAdapter("/"))
                 .filter(GenericSelector<String> {
                     try {
                         (it.toInt()) % 3 == 0
@@ -48,35 +42,8 @@ class FileUploadApplication {
                 )
                 .get()
     }
-
-    /*
-    @Bean
-    fun output(): QueueChannelSpec? {
-        return MessageChannels.queue("output")
-    }
-
-    @Bean
-    fun buildHttpSupport(): HttpRequestHandlingEndpointSupport {
-        return this.build()!!.get()
-    }
-
-    fun build(): HttpRequestHandlerEndpointSpec? {
-        return Http.inboundChannelAdapter("/")
-                .requestMapping { m -> m.methods(HttpMethod.POST)
-                        .consumes(MediaType.MULTIPART_FORM_DATA_VALUE) }
-                .requestChannel("output")
-    }
-     */
 }
 
 fun main(args: Array<String>) {
-    val context = runApplication<FileUploadApplication>(*args)
-
-    println(">>>>> sending message ... of type Int")
-    val channel = context.getBean("numChannel", MessageChannel::class.java)
-
-    (1..100)
-            .map { it.toString() }
-            .map { MessageBuilder.withPayload(it).build() }
-            .forEach { channel.send(it) }
+    runApplication<FileUploadApplication>(*args)
 }
